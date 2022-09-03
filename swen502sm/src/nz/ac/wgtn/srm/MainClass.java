79e3061cb966526ffd4f3d7ae32bca9c3d76a1f3
Stuart Marshall <stuartrichardmarshall@gmail.com>
2022-09-03 13:05:57 +1200

    Echo command line args to console.

---
+++ swen502sm/src/nz/ac/wgtn/srm/MainClass.java
@@ -0,0 +1,16 @@
+package nz.ac.wgtn.srm;
+
+public class MainClass {
+
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		for (int index = 0; index < args.length; index++) {
+			System.out.println("arg[" + index + "] : " + args[index]);
+		}
+
+	}
+
+}
